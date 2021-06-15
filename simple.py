5cee12179f1ac7969cff5cb9dc240d20e6312890
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-06-15 14:38:32 +0200

    Implemented simple Python operator

--- simple.py
+++ simple.py
@@ -1,16 +1,19 @@
 import datetime as dt
 
 from airflow import DAG
-from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.operators.python import PythonOperator
+
+
+def _connect_to_irods():
+    print('Connecting to IRODS...')
 
 
 with DAG(
-        dag_id='simple',
-        description='Simple DAG that connects to XNAT',
+        dag_id='irods_to_xnat',
+        description='Simple DAG that connects to IRODS',
         start_date=dt.datetime(2021, 6, 15),
 ) as dag:
-    connect = DockerOperator(
-        task_id='connect',
-        image='brecheisen/simple-connect:latest',
-        command='connect.py',
+    connect_to_irods = PythonOperator(
+        task_id='connect_to_irods',
+        python_callable=_connect_to_irods,
     )
--- simple/connect/connect.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('running task connect...')
-
-
-if __name__ == '__main__':
-    main()
