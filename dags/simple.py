da3a5bbafe041944409d907f46088b793a529ef5
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-06-15 15:24:04 +0200

    Refactored project a bit

--- dags/simple.py
+++ dags/simple.py
@@ -4,14 +4,15 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 
+def _connect_to_irods():
+    print('Connecting to IRODS...')
+
+
 with DAG(
         dag_id='irods_to_xnat',
         description='Simple DAG that connects to IRODS',
         start_date=dt.datetime(2021, 6, 15),
 ) as dag:
-
-    def _connect_to_irods():
-        print('Connecting to IRODS...')
 
     my_operator = PythonOperator(
         task_id='connect_to_irods',
